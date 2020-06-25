c3d759c3b12168418660ebe4b4d65296dfec94fd
josemiguelq <jose.miguelq95@gmail.com>
2020-06-24 22:03:23 -0400

    finalizando laravel 7

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -35,7 +35,5 @@ class Kernel extends ConsoleKernel
     protected function commands()
     {
         $this->load(__DIR__.'/Commands');
-
-        require base_path('routes/console.php');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::post('/transaction', function (Request $request) {
     return $request->user();
 });
