938ebaaa9aa6f79d5eacda763c65ff2f538c5467
Joubert Guimarães de Assis <eu+github@redrat.com.br>
2020-07-05 00:41:39 -0300

    add orm

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     RedRat\ApiHelperBundle\ApiHelperBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
