c0b9f0e1f7afcdeee3dc858ee2f0265431ce688a
Joubert Guimarães de Assis <eu+github@redrat.com.br>
2020-07-05 18:41:21 -0300

    Create fixtures

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     RedRat\ApiHelperBundle\ApiHelperBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
