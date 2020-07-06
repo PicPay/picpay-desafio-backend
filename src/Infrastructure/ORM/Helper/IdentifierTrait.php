26e298d88fdd9b1012eb855c4afe8f67877433b4
Joubert Guimarães de Assis <eu+github@redrat.com.br>
2020-07-05 23:54:23 -0300

    Remove int id as key

--- src/Infrastructure/ORM/Helper/IdentifierTrait.php
+++ src/Infrastructure/ORM/Helper/IdentifierTrait.php
@@ -8,27 +8,16 @@ use Ramsey\Uuid\Uuid;
 
 trait IdentifierTrait
 {
-    protected int $id;
     protected string $uuid;
-
-    public function getId(): int
-    {
-        return $this->id;
-    }
 
     public function getUuid(): string
     {
         return $this->uuid;
     }
 
-    public function setUuid(string $uuid): void
-    {
-        $this->uuid = $uuid;
-    }
-
     public function forgeUuid(): void
     {
-        $this->setUuid(self::generateUuidV4());
+        $this->uuid = self::generateUuidV4();
     }
 
     public static function generateUuidV4(): string
