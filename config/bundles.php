a10c87b52392d5c7bd56c700a011dee32173c97f
Joubert Guimarães de Assis <eu+github@redrat.com.br>
2020-07-07 02:30:54 -0300

    Finish external api request

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    EightPoints\Bundle\GuzzleBundle\EightPointsGuzzleBundle::class => ['all' => true],
 ];
--- src/Infrastructure/Domain/Transaction/Component/Vendor/AuthorizerFooBar/ApiClient/ApiClient.php
+++ src/Infrastructure/Domain/Transaction/Component/Vendor/AuthorizerFooBar/ApiClient/ApiClient.php
@@ -6,11 +6,41 @@ namespace App\Infrastructure\Domain\Transaction\Component\Vendor\AuthorizerFooBa
 
 use App\Domain\Shared\ValueObject\Document;
 use App\Domain\Transaction\Component\Vendor\AuthorizerFooBar\ApiClient\ApiClientInterface;
+use Fig\Http\Message\RequestMethodInterface;
+use GuzzleHttp\ClientInterface;
+use Throwable;
+
+use function json_decode;
 
 class ApiClient implements ApiClientInterface
 {
+    public const RESPONSE_AUTHORIZED = 'Autorizado';
+
+    private ClientInterface $client;
+
+    public function __construct(ClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
     public function isValidPayerAccount(Document $document): bool
     {
-        return false;
+        try {
+            $response = $this
+                ->client
+                ->request(RequestMethodInterface::METHOD_GET, '')
+            ;
+
+            $apiData = json_decode(
+                $response
+                    ->getBody()
+                    ->getContents(),
+                true
+            );
+
+            return self::RESPONSE_AUTHORIZED === $apiData['message'];
+        } catch (Throwable $e) {
+            return false;
+        }
     }
 }
