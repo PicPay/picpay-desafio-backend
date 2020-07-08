47e7833e8af7e69c8103883053a3f532c107fdb1
Joubert Guimarães de Assis <eu+github@redrat.com.br>
2020-07-08 13:04:21 -0300

    Ajustments into entrypoint to support coverage

--- scripts/docker/app-init/entrypoint.sh
+++ scripts/docker/app-init/entrypoint.sh
@@ -3,3 +3,7 @@
 php /var/www/html/bin/console doctrine:database:create
 php /var/www/html/bin/console doctrine:migrations:migrate -q
 php /var/www/html/bin/console doctrine:fixtures:load -q
+
+php /var/www/html/composer.phar composer run coverage
+
+cp /var/www/html/tests/_reports/unit/coverage /var/www/html/public/coverage -Rf
