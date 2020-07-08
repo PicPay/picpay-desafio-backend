30faf4d2eb7caa0e669d567568685b52cd5321ce
Vitor Paiva <vitor.albuquerque@veus.com.br>
2020-07-08 10:36:44 -0300

    Modificacoes no model User

--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -1,12 +1,11 @@
 <?php
 
-namespace App;
+namespace App\Models\User;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class Users extends Authenticatable
 {
     use Notifiable;
 
@@ -16,7 +15,9 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
     ];
 
     /**
@@ -25,7 +26,8 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
 
     /**
