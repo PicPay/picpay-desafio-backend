efe46fff1312f6f9831ce33504d944c75b86fa54
Vitor Paiva <vitor.albuquerque@veus.com.br>
2020-07-08 10:37:43 -0300

    Inicio do esboco dos testes

--- tests/Unit/Transfer/TestMoneyTransfer.php
+++ tests/Unit/Transfer/TestMoneyTransfer.php
@@ -4,15 +4,16 @@ namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
-class ExampleTest extends TestCase
+class TestMoneyTransfer extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testMoneyTransfer()
     {
+
         $this->assertTrue(true);
     }
 }
