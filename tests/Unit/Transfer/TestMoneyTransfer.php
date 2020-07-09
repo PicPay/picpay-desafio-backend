884cf3f8dd809cfc32f2ac1b4655411c5f623e96
Vitor Paiva <vitor.albuquerque@veus.com.br>
2020-07-09 00:00:08 -0300

    Teste de transferencia

--- tests/Unit/Transfer/TestMoneyTransfer.php
+++ tests/Unit/Transfer/TestMoneyTransfer.php
@@ -1,19 +1,23 @@
 <?php
 
-namespace Tests\Unit;
+namespace Tests\Unit\Transfer;
 
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
 
 class TestMoneyTransfer extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testMoneyTransfer()
+    public function testMoneyTransfer(): void
     {
-        
-        $this->assertTrue(true);
+        $this->withoutExceptionHandling();
+        $data = [
+            "value" => 100.00,
+            "payer_id" => 1,
+            "payee_id" => 2
+        ];
+        $response = $this->postJson('/api/transfer', $data);
+        $response->assertOk();
+        $this->assertDatabaseCount('transfer', 1);
+        $data['authorization_status']=1;
+        $this->assertDatabaseHas('transfer', $data);
     }
 }
