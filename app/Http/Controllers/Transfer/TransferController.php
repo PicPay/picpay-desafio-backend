39bea32e36d5f779cbc6fddb425e28c3b15e5ef3
Vitor Paiva <vitor.albuquerque@veus.com.br>
2020-07-08 23:59:25 -0300

    Refactoring do TransferController

--- app/Http/Controllers/Transfer/TransferController.php
+++ app/Http/Controllers/Transfer/TransferController.php
@@ -3,39 +3,26 @@
 namespace App\Http\Controllers\Transfer;
 
 use App\Http\Controllers\Controller;
-use App\Repositories\Contracts\Transfer\TransferRepositoryContract;
-use App\Services\Contracts\Authorization\AuthorizationServiceContract;
+use App\Services\Contracts\Transfer\TransferServiceContract;
 use Illuminate\Http\Request;
 
 class TransferController extends Controller
 {
+
     /**
-     * @var TransferRepositoryContract
+     * @var TransferServiceContract
      */
-    private $transferRepository;
-    /**
-     * @var AuthorizationServiceContract
-     */
-    private $authorizationService;
+    private $transferService;
 
     public function __construct(
-        TransferRepositoryContract $transferRepository,
-        AuthorizationServiceContract $authorizationService
+        TransferServiceContract $transferService
     ) {
-        $this->transferRepository = $transferRepository;
-        $this->authorizationService = $authorizationService;
+        $this->transferService = $transferService;
     }
 
     public function __invoke(Request $request)
     {
-        $transfer = $this->transferRepository->createTransfer($request->payer_id, $request->payee_id, $request->value);
-        if ($this->authorizationService->isTransferAuthorized(
-            $request->payer_id,
-            $request->payee_id,
-            $request->value
-        )) {
-            $this->transferRepository->setTransferAsAuthorized($transfer->id);
-        }
+        $this->transferService->processTransfer($request->payer_id, $request->payee_id, $request->value);
         return [];
     }
 }
