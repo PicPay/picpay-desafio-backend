127bee8425ea3bb0e52f367d6f95541d319a8637
Vitor Paiva <vitor.albuquerque@veus.com.br>
2020-07-08 23:06:00 -0300

    Adicao dos services providers

--- config/app.php
+++ config/app.php
@@ -174,6 +174,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\Transfer\TransferRepositoryServiceProvider::class,
+        App\Providers\Authorization\AuthorizationServiceProvider::class,
 
     ],
 
