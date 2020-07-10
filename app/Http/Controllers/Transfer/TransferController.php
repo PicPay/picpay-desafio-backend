cb4bf6d74ebe3c4088a0949b04783fb0980daad1
Vitor Paiva <vitor.albuquerque@veus.com.br>
2020-07-10 01:23:47 -0300

    Adicao de exception handling

--- app/Http/Controllers/Transfer/TransferController.php
+++ app/Http/Controllers/Transfer/TransferController.php
@@ -10,7 +10,19 @@ class TransferController extends Controller
 {
     public function __invoke(ValidateTransferRequest $request, TransferServiceContract $transferService)
     {
-        $transferService->registerTransfer($request->payer_id, $request->payee_id, $request->value);
-        return [];
+        try {
+            $transfer = $transferService->registerTransfer($request->payer_id, $request->payee_id, $request->value);
+            $result = [
+                'status' => true,
+                'transfer' => $transfer
+            ];
+            return response()->json($result);
+        } catch (Exception $e) {
+            $result = [
+                'status' => false,
+                'error' => $e->getMessage()
+            ];
+            return response()->json($result, $e->getCode());
+        }
     }
 }
