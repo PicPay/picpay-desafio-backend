8e015061c6cfb28ae1a72a9b7090daaff940be24
Vitor Paiva <vitor.albuquerque@veus.com.br>
2020-07-10 08:36:03 -0300

    Renomeando rotas

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::post('transaction', 'Transfer\TransferController')->name('transfer');
+Route::post('transaction', 'Transfer\TransferController')->name('transaction');
 
