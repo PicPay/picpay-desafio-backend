641f8c2eb68c36739cbe046c39d9c1e05099ce5a
Vitor Paiva <vitor.albuquerque@veus.com.br>
2020-07-10 01:24:19 -0300

    Refactor da Authorization Service

--- app/Services/Authorization/AuthorizationService.php
+++ app/Services/Authorization/AuthorizationService.php
@@ -7,19 +7,9 @@ use Illuminate\Support\Facades\Http;
 
 class AuthorizationService implements AuthorizationServiceContract
 {
-    /**
-     * @var Http
-     */
-    private $client;
-
-    public function __construct(Http $client)
-    {
-        $this->client = $client;
-    }
-
     public function isTransferAuthorized($payer_id, $payee_id, $value)
     {
-        $result = $this->client::get('https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6');
+        $result = Http::get('https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6');
         if (isset($result['message'])) {
             return $result['message']==='Autorizado';
         }
