4bf8386549d541bfa125ade2c4405e598db3b7b5
Vitor Paiva <vitor.albuquerque@veus.com.br>
2020-07-10 01:25:24 -0300

    Refactor da Authorization Service

--- app/Services/Authorization/AuthorizationService.php
+++ app/Services/Authorization/AuthorizationService.php
@@ -9,10 +9,16 @@ class AuthorizationService implements AuthorizationServiceContract
 {
     public function isTransferAuthorized($payer_id, $payee_id, $value)
     {
-        $result = Http::get('https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6');
-        if (isset($result['message'])) {
-            return $result['message']==='Autorizado';
+        try {
+            $result = Http::get('https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6');
+            if (isset($result['message'])) {
+                return $result['message']==='Autorizado';
+            }
+            return false;
         }
-        return false;
+        catch(\Exception $e){
+            return false;
+        }
+
     }
 }
