1caa82af4c65f3bce1484fe4179f763b6f957b06
Vitor Paiva <vitor.albuquerque@veus.com.br>
2020-07-10 00:25:16 -0300

    Adicao dos events para monitoramento

--- app/Models/Transfer/Transfer.php
+++ app/Models/Transfer/Transfer.php
@@ -7,5 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Transfer extends Model
 {
     protected $table = 'transfer';
-    protected $fillable = ['payer_id', 'payee_id', 'value'];
+    protected $fillable = ['payer_id', 'payee_id', 'value', 'cancelled', 'processed', 'authorization_status'];
+    protected $observables = [
+        'authorized',
+        'cancelled',
+    ];
+
+    public function authorized()
+    {
+        $this->fireModelEvent('authorized', false);
+    }
+
+    public function cancelled()
+    {
+        $this->fireModelEvent('cancelled', false);
+    }
 }
