fbbd30277b9d4ce721f4b792cafed06e2a92d03a
Kaique Garcia <kaiquegarciam@gmail.com>
2020-07-11 16:11:45 -0300

    Ajuste na classe padrão User para funcionar como especialização da classe Person

--- laravel/app/User.php
+++ laravel/app/User.php
@@ -2,38 +2,17 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use App\Concepts\Person;
+use App\Enums\PersonTypeEnum;
 
-class User extends Authenticatable
+class User extends Person
 {
-    use Notifiable;
+    public static function boot()
+    {
+        parent::boot();
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+        static::addGlobalScope(function ($query) {
+            $query->where("type", PersonTypeEnum::USER);
+        });
+    }
 }
