1c3f9f27ef54e4f526cd1fd28118bac7417d3fe8
JhonatanHenrique <jhonatan.henrique@fcmoreno.com.br>
2020-07-11 17:17:18 -0300

    Configurando a tabela de usuários

--- app/User.php
+++ app/User.php
@@ -18,6 +18,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'user_type_id',
+        'document',
         'email',
         'password',
     ];
--- database/migrations/2020_07_11_185035_create_users_table.php
+++ database/migrations/2020_07_11_185035_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->foreignId('user_type_id')->constrained('user_types');
+            $table->string('document', 18)->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
