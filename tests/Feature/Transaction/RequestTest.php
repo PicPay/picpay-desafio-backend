4a98d441d68602cc31918c6f1653b86ab4cbd15c
Domício Martins <domiciom@gmail.com>
2020-07-12 14:35:18 -0300

    Adicionando os testes de integração

---
+++ tests/Feature/Transaction/RequestTest.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Tests\Feature\Transaction;
+
+use Tests\TestCase;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Symfony\Component\HttpFoundation\Response;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+
+class RequestTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    const TRANSACTION_API_URL = '/api/v1/transaction';
+
+    const DEFAULT_METHOD = "POST";
+
+    private $payload;
+
+    /**
+     *  Setup for payload initialize.
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->payload = [
+            'payer' => 0,
+            'payee' => 0,
+            'value' => 1
+        ];
+    }
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testProhibitedMethodValidation(): void
+    {
+        $response = $this->call("PUT",self::TRANSACTION_API_URL, $this->payload);
+
+        $this->assertEquals(Response::HTTP_METHOD_NOT_ALLOWED, $response->status());
+    }
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testGeneralValidation(): void
+    {
+        $response = $this->call(self::DEFAULT_METHOD,self::TRANSACTION_API_URL, $this->payload);
+
+        $this->assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $response->status());
+    }
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testOnlyCommonUserPayer(): void
+    {
+        $this->payload["payer"] = $this->createShopkeeper()->id;
+        $this->payload["payee"] = $this->createCommonUser()->id;
+
+        $response = $this->call(self::DEFAULT_METHOD,self::TRANSACTION_API_URL, $this->payload);
+
+        $this->assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $response->status());
+    }
+
+    /**
+     * 
+     * 
+     * @return void
+     */
+    public function testExistsPayeeId(): void
+    {
+        $this->payload["payer"] = $this->createCommonUser()->id;
+        $this->payload["payee"] = 0;
+
+        $response = $this->call(self::DEFAULT_METHOD,self::TRANSACTION_API_URL, $this->payload);
+
+        $this->assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $response->status());
+    }
+
+    /**
+     * 
+     * 
+     * @return void
+     */
+    public function testNegativeValue(): void
+    {
+        $this->payload["payer"] = $this->createCommonUser()->id;
+        $this->payload["payee"] = $this->createShopkeeper()->id;
+        $this->payload["value"] = -1;
+
+        $response = $this->call(self::DEFAULT_METHOD,self::TRANSACTION_API_URL, $this->payload);
+
+        $this->assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $response->status());
+    }
+
+    /**
+     * 
+     * 
+     * @return void
+     */
+    public function testNotNumberValue(): void
+    {
+        $this->payload["payer"] = $this->createCommonUser()->id;
+        $this->payload["payee"] = $this->createCommonUser()->id;
+        $this->payload["value"] = "picpay";
+
+        $response = $this->call(self::DEFAULT_METHOD,self::TRANSACTION_API_URL, $this->payload);
+
+        $this->assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $response->status());
+    }
+}
---
+++ tests/Feature/Transaction/TransferTest.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace Tests\Feature\Transaction;
+
+use Tests\TestCase;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Symfony\Component\HttpFoundation\Response;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+use App\Events\Transaction\Validation;
+use App\Interfaces\Transaction\TransactionInterface;
+use App\Interfaces\User\UserWalletInterface;
+use App\Models\Transaction\Transaction;
+use App\Listeners\Transaction\Authorization;
+use Mockery;
+
+class TransferTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    const TRANSACTION_API_URL = '/api/v1/transaction';
+
+    const TRANSACTION_TABLE = "transactions";
+    const USER_WALLET_TABLE = "users_wallet";
+
+    const DEFAULT_METHOD = "POST";
+
+    private $payload;
+
+    private $commonUser;
+    private $shopKeeper;
+
+    private $transcationRepository;
+    private $walletRepository;
+
+    /**
+     *  Setup for payload initialize.
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->payload = [
+            'payer' => $this->createCommonUser()->user_id,
+            'payee' => $this->createShopKeeper()->user_id,
+            'value' => 90
+        ];
+
+        $this->transcationRepository = app(TransactionInterface::class);
+        $this->walletRepository = app(UserWalletInterface::class);
+    }
+
+    /**
+     * Testing transaction intent of transfer credits
+     *
+     * @return void
+     */
+    public function testTransactionIntent(): void
+    {
+        $response = $this->call(self::DEFAULT_METHOD,self::TRANSACTION_API_URL, $this->payload);
+
+        $this->seeInDatabase(self::TRANSACTION_TABLE, $this->payload);
+    }
+
+    /**
+     *  Transaction fire event test
+     *
+     * @return void
+     */
+    public function testTransactionEvent(): void
+    {
+        $this->expectsEvents(Validation::class);
+        
+        $response = $this->call(self::DEFAULT_METHOD,self::TRANSACTION_API_URL, $this->payload);
+
+    }
+
+    /**
+     *  Testing transaction success between two users
+     *
+     * @return void
+     */
+    public function testTransactionSucessful(): void
+    {
+        $response = $this->call(self::DEFAULT_METHOD,self::TRANSACTION_API_URL, $this->payload);
+
+        $this->payload["status"] = Transaction::APPROVED;
+
+        $this->seeInDatabase(self::TRANSACTION_TABLE, $this->payload);
+    }
+
+    /**
+     * Testing money increase on user wallet
+     *
+     * @return void
+     */
+    public function testImproveUserWallet(): void
+    {
+        $payerWalletBefore = $this->walletRepository->findByUserId($this->payload["payer"]);
+        $payeeWalletBefore = $this->walletRepository->findByUserId($this->payload["payee"]);
+        
+        $response = $this->call(self::DEFAULT_METHOD,self::TRANSACTION_API_URL, $this->payload);
+
+        $payeeWalletBefore["amount"] += $this->payload["value"];
+
+        $this->removeDateFields($payeeWalletBefore);
+
+        $this->seeInDatabase(self::USER_WALLET_TABLE, $payeeWalletBefore);
+    }
+
+    /**
+     * Testing money descrease on user wallet
+     *
+     * @return void
+     */
+    public function testDescreaseUserWalletAmount(): void
+    {
+        $payerWalletBefore = $this->walletRepository->findByUserId($this->payload["payer"]);
+        
+        $response = $this->call(self::DEFAULT_METHOD,self::TRANSACTION_API_URL, $this->payload);
+
+        $payerWalletBefore["amount"] -= $this->payload["value"];
+
+        $this->removeDateFields($payerWalletBefore);
+
+        $this->seeInDatabase(self::USER_WALLET_TABLE, $payerWalletBefore);
+    }
+
+    /**
+     *
+     *
+     * @return void
+     */
+    public function testDeclinedTransaction(): void
+    {
+        $this->changeAuthorizationService();
+
+        $response = $this->call(self::DEFAULT_METHOD,self::TRANSACTION_API_URL, $this->payload);
+
+        $this->payload["status"] = Transaction::INCONSISTENCY;
+
+        $this->seeInDatabase(self::TRANSACTION_TABLE, $this->payload);
+    }
+
+    /**
+     * Setting to a non existant service
+     *
+     * @return void
+     */
+    private function changeAuthorizationService(): void
+    {
+        config(['external.authorization' => 'http://en.c1']);
+    }
+}
