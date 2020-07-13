eaa44c39867f90155e42288ce65e334364bbf3f5
Esdras Rios <esdrasrioos@gmail.com>
2020-07-13 12:00:39 -0300

    initial commit lumen boilerplate

---
+++ api/tests/TestCase.php
@@ -0,0 +1,16 @@
+<?php
+
+use Laravel\Lumen\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    /**
+     * Creates the application.
+     *
+     * @return \Laravel\Lumen\Application
+     */
+    public function createApplication()
+    {
+        return require __DIR__.'/../bootstrap/app.php';
+    }
+}
