1d88a04a24b7dd231216e0a1d89ae2c45301c84d
raabe <raabe@explicae.com.br>
2020-07-19 16:54:55 -0300

    Add midddlerware

--- picpay-desafio-backend/routes/api.php
+++ picpay-desafio-backend/routes/api.php
@@ -11,26 +11,32 @@ use Illuminate\Support\Facades\Route;
 
 /** @var Router $router */
 
-$router->group(['prefix' => 'users'], function () use ($router) {
-    $router->get('', 'UserController@index');
-    $router->get('{id}', 'UserController@show');
-    $router->post('', 'UserController@store');
-    $router->put('{id}', 'UserController@update');
-    $router->delete('{id}', 'UserController@destroy');
+$router->middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
 });
 
-$router->group(['prefix' => 'wallets'], function () use ($router) {
-    $router->get('', 'WalletController@index');
-    $router->get('{id}', 'WalletController@show');
-    $router->post('', 'WalletController@store');
-    $router->put('{id}', 'WalletController@update');
-    $router->delete('{id}', 'WalletController@destroy');
-});
+$router->group(['middleware' => 'api'], function () use ($router){
+    $router->group(['prefix' => 'users'], function () use ($router) {
+        $router->get('', 'UserController@index');
+        $router->get('{id}', 'UserController@show');
+        $router->post('', 'UserController@store');
+        $router->put('{id}', 'UserController@update');
+        $router->delete('{id}', 'UserController@destroy');
+    });
 
-$router->group(['prefix' => 'transactions'], function () use ($router) {
-    $router->get('', 'TransactionController@index');
-    $router->get('{id}', 'TransactionController@show');
-    $router->post('', 'TransactionController@store');
-    $router->put('{id}', 'TransactionController@update');
-    $router->delete('{id}', 'TransactionController@destroy');
+    $router->group(['prefix' => 'wallets'], function () use ($router) {
+        $router->get('', 'WalletController@index');
+        $router->get('{id}', 'WalletController@show');
+        $router->post('', 'WalletController@store');
+        $router->put('{id}', 'WalletController@update');
+        $router->delete('{id}', 'WalletController@destroy');
+    });
+
+    $router->group(['prefix' => 'transactions'], function () use ($router) {
+        $router->get('', 'TransactionController@index');
+        $router->get('{id}', 'TransactionController@show');
+        $router->post('', 'TransactionController@store');
+        $router->put('{id}', 'TransactionController@update');
+        $router->delete('{id}', 'TransactionController@destroy');
+    });
 });
