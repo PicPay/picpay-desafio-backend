babd0810efd0de92d2638b519c088ce54e382abb
raabe <raabe@explicae.com.br>
2020-07-19 00:05:56 -0300

    Add rotas de wallet

--- picpay-desafio-backend/routes/api.php
+++ picpay-desafio-backend/routes/api.php
@@ -18,3 +18,11 @@ $router->group(['prefix' => 'users'], function () use ($router) {
     $router->put('{id}', 'UserController@update');
     $router->delete('{id}', 'UserController@destroy');
 });
+
+$router->group(['prefix' => 'wallets'], function () use ($router) {
+    $router->get('', 'WalletController@index');
+    $router->get('{id}', 'WalletController@show');
+    $router->post('', 'WalletController@store');
+    $router->put('{id}', 'WalletController@update');
+    $router->delete('{id}', 'WalletController@destroy');
+});
