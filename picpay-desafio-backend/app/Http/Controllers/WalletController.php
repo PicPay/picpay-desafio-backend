a8232ffc88f4a28aa73ae6522a04e48f2c1938a3
raabe <raabe@explicae.com.br>
2020-07-20 00:18:32 -0300

    Add criação da carteira vinculada a usuário

--- picpay-desafio-backend/app/Http/Controllers/WalletController.php
+++ picpay-desafio-backend/app/Http/Controllers/WalletController.php
@@ -2,29 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\UserRequest;
-use App\Models\User;
 use App\Models\Wallet;
-use Illuminate\Support\Facades\DB;
 
 class WalletController extends BaseController
 {
     public function __construct()
     {
         $this->classModel = Wallet::class;
-    }
-
-    public final function createWallet(User $user, UserRequest $request)
-    {
-        try {
-          DB::transaction(function () use ($user, $request) {
-                $wallet = new Wallet();
-                $wallet->user_id = $user->id;
-                $wallet->balance = 0.0;
-                $wallet->saveOrFail();
-            });
-        }catch (Throwable $error){
-            return response()->json(['message' => $error->getMessage()], 400);
-        }
     }
 }
--- picpay-desafio-backend/app/Models/Wallet.php
+++ picpay-desafio-backend/app/Models/Wallet.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use App\Http\Requests\UserRequest;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use \Throwable;
 
 class Wallet extends Model
 {
     protected $fillable = ['user_id', 'balance'];
-    protected $appends  = ['id'];
 
     public function user()
     {
@@ -17,5 +19,34 @@ class Wallet extends Model
     public function transaction()
     {
         return $this->hasMany('App\Models\Transaction');
+    }
+
+    public function hasBalance(float $value)
+    {
+        return $this->balance >= $value;
+    }
+
+    public final function createWallet(User $user, UserRequest $request)
+    {
+        try {
+            DB::transaction(function () use ($user, $request) {
+                $wallet = new Wallet();
+                $wallet->user_id = $user->id;
+                $wallet->balance = 0.0;
+                $wallet->saveOrFail();
+            });
+        }catch (Throwable $error){
+            return $error->getMessage();
+        }
+    }
+
+    public function addBalance(float $value)
+    {
+        $this->balance += $value;
+    }
+
+    public function debitBalance(float $value)
+    {
+        $this->balance -= $value;
     }
 }
