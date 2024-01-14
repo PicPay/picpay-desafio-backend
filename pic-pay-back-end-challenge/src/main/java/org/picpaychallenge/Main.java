fe6e20b1cfeae750cc572943aea82e72c6c8c010
Ellyan Fernandes <ellyan.fernandes@hotmail.com>
2024-01-14 08:56:14 -0300

    First commit

---
+++ pic-pay-back-end-challenge/src/main/java/org/picpaychallenge/Main.java
@@ -0,0 +1,10 @@
+package org.picpaychallenge;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
