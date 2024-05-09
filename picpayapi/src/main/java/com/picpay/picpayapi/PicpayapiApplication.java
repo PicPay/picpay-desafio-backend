82e05acb32b3b2d88e9f134477cd892908baaecd
MarconeRafael <50372440+superkarlos@users.noreply.github.com>
2024-05-09 15:40:41 -0300

    Arquivos padrão mavem

---
+++ picpayapi/src/main/java/com/picpay/picpayapi/PicpayapiApplication.java
@@ -0,0 +1,13 @@
+package com.picpay.picpayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayapiApplication.class, args);
+	}
+
+}
---
+++ picpayapi/src/test/java/com/picpay/picpayapi/PicpayapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpay.picpayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
