23c45bbd01641317631b69e0f229a861d7a1ab66
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2024-08-13 22:10:46 -0300

    feat: adding project structure

---
+++ src/main/java/com/challenge/picpay/PicpayChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.picpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/picpay/PicpayChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
