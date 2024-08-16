43e01c676c7164cd60f1faaa69a3498c210836ad
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2024-08-16 17:35:51 -0300

    feat: adding trigger for created at and is deleted

--- src/main/resources/db/migration/V1__create_user_tb.sql
+++ src/main/resources/db/migration/V1__create_user_tb.sql
@@ -5,7 +5,8 @@ CREATE TABLE IF NOT EXISTS user_tb (
                                        user_email VARCHAR(255) NOT NULL UNIQUE,
                                        user_password VARCHAR(255) NOT NULL,
                                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                                       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                                       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                       is_deleted BOOLEAN NOT NULL
 );
 
 CREATE INDEX IF NOT EXISTS idx_user_code ON user_tb(user_code);
@@ -18,7 +19,39 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+-- UPDATED AT TRIGGER
+
 CREATE TRIGGER trg_update_user_tb
     BEFORE UPDATE ON user_tb
     FOR EACH ROW
 EXECUTE FUNCTION update_user_tb_updated_at();
+
+-- CREATED AT TRIGGER
+
+CREATE OR REPLACE FUNCTION update_user_tb_created_at()
+    RETURNS TRIGGER AS $$
+BEGIN
+    NEW.created_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_create_user_tb
+    BEFORE INSERT ON user_tb
+    FOR EACH ROW
+EXECUTE FUNCTION update_user_tb_created_at();
+
+-- DELETED TRIGGER
+
+CREATE OR REPLACE FUNCTION update_user_tb_is_deleted()
+    RETURNS TRIGGER AS $$
+BEGIN
+    NEW.is_deleted = FALSE;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_create_deleted_prop_user_tb
+    BEFORE INSERT ON user_tb
+    FOR EACH ROW
+EXECUTE FUNCTION update_user_tb_is_deleted();
